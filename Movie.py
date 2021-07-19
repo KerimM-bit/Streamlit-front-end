1a6db9f76346d1d51f65d5ae48b921d8ef8ec16e
User <kmirzeyev30@gmail.com>
2021-07-19 15:25:07 +0400

    it is done

--- Movie.py
+++ Movie.py
@@ -13,10 +13,10 @@ import plotly.graph_objects as go
 
 header = st.beta_container()
 dataset = st.beta_container()
-features = st.beta_container()
 #model_training = st.beta_container()
-linear_reg = st.beta_container()
 interactive = st.beta_container()
+features = st.beta_container()
+linear_reg = st.beta_container()
 
 st.markdown(
     """
@@ -42,28 +42,57 @@ with header:
     
 with dataset:
     st.header('Movie dataset')
-    st.text('You can access this dataset via my github or bla saytindan.')
+    st.text('You can access this dataset from : `https://www.kaggle.com/rounakbanik/the-movies-dataset`.')
+    
     
     data = get_data("E:\Data analyst\Streamlit-data_viz\data\movie_metadata.csv")
+    st.write(data)
+    st.write('DataFrame dimensions:', data.shape)
+  
+
+    
+    
+    tab_info = pd.DataFrame(data.dtypes).T.rename(index={0:'column type'})
+    tab_info = tab_info.append(pd.DataFrame(data.isnull().sum()).T.rename(index={0:'null_values'}))
+    tab_info = tab_info.append(pd.DataFrame(data.isnull().sum()/data.shape[0]*100).T.
+                               rename(index={0:'null values (%)'}))
+    
     
     
     changed_ds = data.dropna(how='any', axis=0)
     changed_ds = changed_ds.drop(['color', 'facenumber_in_poster'], axis=1)
     changed_ds = changed_ds[changed_ds.loc[:]!=0].dropna()
-    st.write(changed_ds)
     
+    changed_ds.drop_duplicates(inplace=True)
 
-    
-    st.subheader('Data cleaning for Linear regression')
-    
+    # Before we apply those we have to get rid of null values
     new_ds = changed_ds[['director_facebook_likes', 'actor_1_facebook_likes',
                         'actor_2_facebook_likes', 'actor_3_facebook_likes', 'imdb_score']].copy()
     
-    #new_ds.head(30)
-    st.write(new_ds.head(5))
+
+    
+    
+
+        
+with features:
+    st.header('The features I created')
+    
+    st.markdown(" **first feature** I created `new_ds` dataframe in order to calculate Linear Regression without having to worry about categorical values.")
+    st.markdown(" **second feature** Next step is to choose dependent and independent variables. In our new created dataframe `imdb_score` will be our dependent variable, other 3 columns will be our independent variables")
+
+    
     
 with interactive:
     st.title('A closer look into the data')
+    
+    st.markdown('Before we  analyze on dataset, we need to learn about how many null values data contains, which columns have them most etc.')
+    
+    st.write(tab_info)
+    st.markdown('Next step is to remove `duplicate values` too.')
+    st.markdown('Number of duplicate values: {}'.format(data.duplicated().sum()))
+    st.write(changed_ds.head(10))
+    st.write('DataFrame dimensions:', changed_ds.shape)
+    st.markdown('Most null values can be found in `gross` and `budget` columns which can be usefull for regressions. If we check dimesions again we can see that data is changed drastically. Also there are some columns we are not going to use for anything so again for better results, we will remove those as well.')
     
     st.header('IMDB links of most liked movies(Top 50)')
     sorted_changed_ds = changed_ds.sort_values(by='imdb_score', ascending=False,
@@ -90,8 +119,32 @@ with interactive:
     #Filtering data
     selected_ds = changed_ds[(changed_ds.country.isin(selected_country))]
     
-    st.header('Display Movies bases on Countries')
+    st.header('Display Movies based on Countries')
     st.dataframe(selected_ds)
+    
+    #choropleth map
+    temp = changed_ds[['movie_title', 'imdb_score', 'country']].groupby(['movie_title', 'imdb_score', 'country']).count()
+    temp = temp.reset_index(drop=False)
+    countries = temp['country'].value_counts()
+    
+    country_data = dict(type='choropleth', locations=countries.index, 
+                        locationmode='country names', z = countries, text=countries.index,
+                        colorbar = {'title':'Movies Number'},
+                        colorscale=[[0,'rgb(224,255,255)'], [0.01,'rgb(166,206,227)'],
+                                    [0.02, 'rgb(31,120,180)'],[0.03,'rgb(178,223,138)'],
+                                    [0.05,'rgb(51,160,44)'], [0.10,'rgb(251,154,153)'],
+                                    [0.20,'rgb(255,255,0)'], [1,'rgb(227,26,28)']],
+                        reversescale=False)
+    layout = dict(geo=dict(showframe=True, projection={'type':'mercator'}))
+    
+    data_map = go.Figure(data=[country_data], layout=layout)
+    data_map.update_layout(margin=dict(l=3,r=3,b=7,t=7), 
+                      paper_bgcolor=background_color) 
+    
+    st.text('Number of movies based on countries.')
+    st.plotly_chart(data_map)
+    
+    st.subheader('From sidebar you can use differend charts to visualize data.')
     
     figure = go.Figure()
     #Violin plot
@@ -132,13 +185,13 @@ with interactive:
         imdb_dist = pd.DataFrame(changed_ds['imdb_score'].value_counts()).head(50)
         st.bar_chart(imdb_dist) 
         
-with features:
-    st.header('The features I created')
-    
-    st.markdown(" **first feature** I created `new_ds` dataframe in order to calculate Linear Regression without having to worry about categorical values.")
-    st.markdown(" **second feature** Next step is to choose dependent and independent variables. In our new created dataframe `imdb_score` will be our dependent variable, other 3 columns will be our independent variables")
-
 with linear_reg:
+    
+    st.subheader('Data cleaning for Linear regression')
+    
+    st.write(new_ds.head(5))
+    
+    
     st.subheader('MultiLinear Regression ')
     #sperate other attributes from the dependent attribute
 
@@ -172,6 +225,7 @@ with linear_reg:
     
     display.subheader('Mean absolute error of the model is: ')
     display.write(mean_absolute_error(y_test,y_prediction))
+    
 
  
 download = st.button('Download CSV File')
